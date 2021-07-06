f2f34e5e2727133b9bc0adec62ef88e6bfe3cf0b
Hiếu Trần <tmhql2k@gmail.com>
2021-07-06 21:23:20 +0700

    almost done profile and edit profie web

--- MayBeYouMissSomeNews/App_Start/BundleConfig.cs
+++ MayBeYouMissSomeNews/App_Start/BundleConfig.cs
@@ -14,20 +14,27 @@ namespace MayBeYouMissSomeNews
                 "~/Content/css/styleRegisterAndLogin.css",
                 "~/Content/css/font-awesome.css"
                 ));
-           
-          /*    bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                           "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/cssProfile").Include(
+                "~/Content/css/style-starter.css",
+                "~/Content/css/styleRegisterAndLogin.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/cssEditProfile").Include(
+                "~/Content/css/argon.css",
+                "~/Content/css/argon.min.css"
+                ));
+            /*    bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                             "~/Scripts/jquery-{version}.js"));
 
-               bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                           "~/Scripts/jquery.validate*"));
+                 bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                             "~/Scripts/jquery.validate*"));
 
-               // Use the development version of Modernizr to develop with and learn from. Then, when you're
-               // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-               bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                           "~/Scripts/modernizr-*"));
+                 // Use the development version of Modernizr to develop with and learn from. Then, when you're
+                 // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+                 bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                             "~/Scripts/modernizr-*"));
 
-               bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                         "~/Scripts/bootstrap.js"));*/
+                 bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                           "~/Scripts/bootstrap.js"));*/
 
 
         }
--- MayBeYouMissSomeNews/Controllers/HomeController.cs
+++ MayBeYouMissSomeNews/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MayBeYouMissSomeNews.Models;
 
 namespace MayBeYouMissSomeNews.Controllers
 {
@@ -13,6 +15,33 @@ namespace MayBeYouMissSomeNews.Controllers
             return View();
         }
 
-       
+        public ActionResult Profile()
+        {
+          
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditProfile(int id)
+        {
+            DBManagerContext context = new DBManagerContext();
+            user user = context.users.SingleOrDefault(p => p.userid == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+        [HttpPost, ActionName("EditProfile")]
+        public ActionResult EditProfile(user user)
+        {
+            DBManagerContext context = new DBManagerContext();
+            user dbUpdate = context.users.FirstOrDefault(p => p.userid == user.userid);
+            if (dbUpdate != null)
+            {
+                context.users.AddOrUpdate(user);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Profile");
+        }
     }
 }
